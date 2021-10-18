8b8545fa52413f6328168482c53ce4f7fbd44464
Nima <nimamuralis@gmail.com>
2021-10-18 18:34:53 +0530

    checked and added class exist ABSPATH

--- plugin_shortcode.php
+++ plugin_shortcode.php
@@ -8,20 +8,30 @@
 * Author URI: https://twitter.com/?lang=en
 **/
 
-
-class Wpdemo{
-	public function __construct(){
-		add_shortcode('welcome', array($this, 'wpb_demo_shortcode'));
-	}
-	public function wpb_demo_shortcode($atts) { 
-		$set_attributes = shortcode_atts( array(
-      	'name' => '',
-      	'color' => 'blue',
-      	'position' =>'center'), $atts );
-      	$value	= '<h1 style="color:'.$set_attributes['color'].';text-align:'.$set_attributes['position'].';">Welcome '.$set_attributes['name'].'</h1>';
-   		return $value;
-	} 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
-$objdemo	= new Wpdemo();
+if ( !class_exists( 'WpDemo' )) {
+	class WpDemo{
+		public function __construct(){
+			add_shortcode('welcome', array($this, 'wpb_demo_shortcode'));
+		}
+		/** 
+		 *create short code to display a text using multiple attributes
+		 *@return type text
+		*/
+		public function wpb_demo_shortcode($atts) { 
+			$set_attributes = shortcode_atts( array(
+	      	'name' => '',
+	      	'color' => 'blue',
+	      	'position' =>'center'), $atts );
+	      	$value	= '<h1 style="color:'.$set_attributes['color'].';text-align:'.$set_attributes['position'].';">Welcome '.$set_attributes['name'].'</h1>';
+	   		return $value;
+		} 
+	}
+	$objdemo	= new WpDemo();
+}else{
+	exit();
+}
 
 ?>
